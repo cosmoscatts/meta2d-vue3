91b51448377a69cd5c13f2d2e655a28757c1408f
cosmoscatts <2138889191@qq.com>
2024-04-12 18:23:41 +0800

    chore: update

--- src/components/props/PropsEquip.vue
+++ src/components/props/PropsEquip.vue
@@ -1,3 +1,185 @@
+<script setup lang="ts">
+import type { Pen } from '@meta2d/core'
+import { equipOptions } from './mock'
+
+const { selections } = useMeta2dSelection()
+
+/**
+ * dataId -> 设备 id
+ * pen tags -> ['equipId', 'hasEquipId']
+ */
+
+const form: any = reactive({
+  equipId: '',
+  id: '',
+  x: 0,
+  y: 0,
+  width: 0,
+  height: 0,
+  image: '', // 图片
+})
+
+function getPen() {
+  const pen = selections.pen as Pen
+  if (!pen)
+    return
+
+  form.equipId = ''
+
+  const tags = pen.tags || []
+  if (tags.includes('hasEquipId') && tags.length === 2) {
+    if (equipOptions.map(i => i.value).includes(tags[0]))
+      form.equipId = tags[0] || ''
+  }
+
+  form.id = pen.id || ''
+
+  const rect = meta2d.getPenRect(pen)
+  if (rect) {
+    form.x = rect.x || 0
+    form.y = rect.y || 0
+    form.width = rect.width || 0
+    form.height = rect.height || 0
+  }
+
+  form.image = pen.image || ''
+}
+
+// 监听选中不同图元
+watch(() => selections.pen?.id, getPen)
+
+const hasSeletedEquip = computed(() => {
+  const equipId = form.equipId || ''
+  if (equipId === '')
+    return false
+  return equipOptions.map(i => i.value).includes(equipId)
+})
+
+const lineDashs = [undefined, [5, 5]]
+
+function changeValue(prop: string) {
+  if (prop === 'equipId') {
+    meta2d.setValue({ id: form.id, dataId: form.equipId, tags: [form.equipId, 'hasEquipId'] }, { render: true })
+    return
+  }
+  const v = { id: form.id } as any
+  v[prop] = form[prop]
+  if (prop === 'dash')
+    v.lineDash = lineDashs[v[prop]]
+
+  meta2d.setValue(v, { render: true })
+}
+
+function changeRect(prop: string) {
+  const v = { id: form.id } as any
+  v[prop] = form[prop]
+  meta2d.setValue(v, { render: true })
+}
+
+async function onBgImageChange(file) {
+  if (!checkImage(file.raw))
+    return
+
+  const imageUrl = await getFileBase64(file.raw)
+  form.image = imageUrl
+  changeValue('image')
+  $message.success('添加背景图片成功')
+}
+
+function checkImage(file) {
+  const isLt2M = file.size / 1024 / 1024 < 2
+  const isImage = file.type.startsWith('image')
+  if (!isImage)
+    $message.error('请上传图片!')
+
+  if (!isLt2M)
+    $message.error('上传图片大小不能超过 2MB!')
+
+  return isImage && isLt2M
+}
+
+onMounted(() => {
+  getPen()
+  meta2d.on('update', getPen)
+  meta2d.on('resizePens', getPen)
+  meta2d.on('rotatePens', getPen)
+  meta2d.on('valueUpdate', getPen)
+})
+</script>
+
 <template>
-  <div>1</div>
+  <div>
+    <el-form :model="form" label-position="right" label-width="80px" size="mini" w-full min-w-190px>
+      <div mb-4 font-bold text-xl>
+        设备
+      </div>
+
+      <el-form-item label="设备">
+        <el-select v-model="form.equipId" style="width: 100px" @change="changeValue('equipId')">
+          <el-option v-for="item in equipOptions" :key="item.value" :value="item.value" :label="item.label" />
+        </el-select>
+      </el-form-item>
+      <Meta2dEquipPropsAutoCreatePoints :equip-id="form.equipId" :has-seleted-equip="hasSeletedEquip" />
+
+      <el-divider />
+
+      <el-form-item label="设备图片">
+        <div relative w-90px h-90px rounded flex-center class="img-wrapper">
+          <img v-show="form.image" :src="form.image" w-full h-full>
+          <div class="img-wrapper-trigger-mask">
+            <el-upload
+              :show-file-list="false" :auto-upload="false" :on-change="onBgImageChange"
+              :before-upload="checkImage"
+            >
+              <i class="el-icon-camera" icon-btn text-18px />
+            </el-upload>
+          </div>
+        </div>
+      </el-form-item>
+
+      <el-divider />
+
+      <el-form-item label="X">
+        <el-input-number v-model="form.x" :controls="false" style="width: 100px" @change="changeRect('x')" />
+      </el-form-item>
+      <el-form-item label="Y">
+        <el-input-number v-model="form.y" :controls="false" style="width: 100px" @change="changeRect('y')" />
+      </el-form-item>
+      <el-form-item label="宽">
+        <el-input-number v-model="form.width" :controls="false" style="width: 100px" @change="changeRect('width')" />
+      </el-form-item>
+      <el-form-item label="高">
+        <el-input-number v-model="form.height" :controls="false" style="width: 100px" @change="changeRect('height')" />
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
+
+<style scoped>
+.img-wrapper {
+  border: 1px dashed #dcdfe6;
+  border-radius: 5px;
+}
+
+.img-wrapper-trigger-mask {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  color: white;
+  font-size: 16px;
+  background-color: #1d212999;
+  border-radius: 5px;
+  opacity: 0;
+  transition: all 0.1s cubic-bezier(0, 0, 1, 1);
+}
+
+.img-wrapper:hover .img-wrapper-trigger-mask {
+  opacity: 1;
+}
+</style>
--- src/components/props/PropsEquipAutoCreatePoints.vue
+++ src/components/props/PropsEquipAutoCreatePoints.vue
@@ -1,3 +1,259 @@
+<script setup lang="ts">
+import { s8 } from '@meta2d/core'
+import { createPointOptions } from './mock'
+import { createBaseCombinePoint, signalInfoMap } from '~/const'
+
+const props = defineProps<{
+  equipId: string
+  hasSeletedEquip: boolean
+}>()
+
+const { selections, select } = useMeta2dSelection()
+
+// 已经创建的测点
+const createdPointInfo = ref<{ key?: string[] }>({}) // { [key]: ['A', 'V', ...] }
+
+const pointOptions = ref<{
+  value: string
+  label: string
+}[]>([])
+
+const ponitsCheckData = ref<string[]>([]) // string[]
+const signalCheckData = ref<{ key?: string[] }>({}) // { [key]: ['A', 'V', ...] }
+
+const pointIdKeyMap = ref({}) // 旧测点数据映射，key - `pointId-signalType` 对应的是 dataId, 需要和 id 做一个绑定，{ key: [ids] }
+
+const selectedPointIds = computed({
+  get() {
+    return ponitsCheckData.value
+  },
+  set(val) {
+    const _signalCheckData = {}
+    Object.keys(signalCheckData.value).forEach((id) => {
+      if (val.includes(id)) {
+        if (!ponitsCheckData.value.includes(id))
+          _signalCheckData[id] = ['A', 'V', 'D', 'T']
+        else
+          _signalCheckData[id] = signalCheckData.value[id]
+      }
+      else {
+        _signalCheckData[id] = []
+      }
+    })
+    signalCheckData.value = _signalCheckData
+    ponitsCheckData.value = val
+  },
+})
+
+const selectedSignalComputedMap = ref({})
+
+function createSelectedSignalComputedMap() {
+  const ids = pointOptions.value.map(i => i.value)
+  const map = {}
+  ids.forEach((id) => {
+    map[id] = computed({
+      get() {
+        return signalCheckData.value[id]
+      },
+      set(val) {
+        if (val.length > 0 && !ponitsCheckData.value.includes(id))
+          ponitsCheckData.value = [...ponitsCheckData.value, id]
+
+        if (!val.length && ponitsCheckData.value.includes(id))
+          ponitsCheckData.value = [...ponitsCheckData.value.filter(i => i !== id)]
+
+        const newSignalCheckData = signalCheckData.value
+        newSignalCheckData[id] = val
+        signalCheckData.value = newSignalCheckData
+      },
+    })
+  })
+  selectedSignalComputedMap.value = map
+}
+
+function setSignalData() {
+  getAllCreatedPoints()
+  const _createdPointInfo = createdPointInfo.value
+  const _createdPointIds = Object.keys(_createdPointInfo)
+  const _signalCheckData = {}
+  const _ponitsCheckData = []
+  pointOptions.value.forEach(({ value }) => {
+    if (_createdPointIds.includes(value)) {
+      _signalCheckData[value] = _createdPointInfo[value]
+      _ponitsCheckData.push(value) // 设置测点
+    }
+    else {
+      _signalCheckData[value] = []
+    }
+  })
+  signalCheckData.value = _signalCheckData
+  ponitsCheckData.value = _ponitsCheckData
+}
+
+watch(() => props.equipId, (n) => {
+  if (n) {
+    const options = createPointOptions(n)
+    pointOptions.value = options
+    setSignalData()
+    createSelectedSignalComputedMap()
+  }
+})
+
+function getAllCreatedPoints() {
+  if (!meta2d || !props.hasSeletedEquip)
+    return
+  const pointPens = meta2d.find('hasPointId')
+  if (!pointPens.length) {
+    createdPointInfo.value = {}
+    pointIdKeyMap.value = {}
+    return
+  }
+
+  const pointPenIds = pointPens.map(pen => pen.id)
+  const currentEquipPoints = meta2d.find(props.equipId).filter(pen => pointPenIds.includes(pen.id))
+  createdPointInfo.value = currentEquipPoints.reduce((prev, cur) => {
+    const tags = cur.tags || []
+    const pointId = tags.length === 3 ? tags[1] : ''
+    if (!pointId)
+      return prev
+    const o = prev[pointId] || []
+    prev[pointId] = [...o, getSignalTypeValue(cur) || ''].filter(i => i !== '')
+    return prev
+  }, {})
+
+  pointIdKeyMap.value = currentEquipPoints.reduce((prev, cur) => {
+    const dataId = cur.dataId
+    if (!dataId)
+      return prev
+    const o = prev[dataId] || []
+    prev[dataId] = [...o, cur.id].filter(Boolean)
+    return prev
+  }, {})
+}
+
+/**
+ * 自动生成测点
+ * 实现：
+ *  - 插入新选中的
+ *  - 删除之前选中现在未选中的
+ */
+function autoGeneratePoints() {
+  // 先统一转换成 'pointId-signalType' 后统一比较
+  const format = (obj) => {
+    return Object.entries(obj).reduce((prev, cur) => {
+      const [key, value] = cur
+      if (value.length) {
+        value.forEach((val) => {
+          prev.push(`${key}-${val}`)
+        })
+      }
+      return prev
+    }, [])
+  }
+
+  const oldSigns = format(createdPointInfo.value)
+  const currentSigns = format(signalCheckData.value)
+
+  const add = []
+  const remove = []
+  oldSigns.forEach((i) => {
+    if (!currentSigns.includes(i))
+      remove.push(i)
+  })
+  currentSigns.forEach((i) => {
+    if (!oldSigns.includes(i))
+      add.push(i)
+  })
+  const addPens = add.map((key, idx) => {
+    const lastDividerIndex = key.lastIndexOf('-')
+    const [pointId, signalType] = [key.slice(0, lastDividerIndex), key.slice(lastDividerIndex + 1)]
+    const pointName = pointOptions.value.find(i => i.value === pointId)?.label || '测点名称'
+    const signalInfo = signalInfoMap[signalType] || signalInfoMap.A
+    const id = s8()
+    const pointObj = createBaseCombinePoint(
+      pointName,
+      signalInfo.nameText,
+      signalInfo.valueText,
+      '时间',
+      { id, dataId: key, tags: [props.equipId, pointId, 'hasPointId'] },
+    )
+    pointObj[0].x = 30 + idx * 10
+    pointObj[0].y = 30
+    pointObj.forEach((o, idx) => {
+      if (idx > 0)
+        o.parentId = id
+    })
+    return pointObj
+  }).flat(2)
+  if (addPens.length)
+    meta2d.addPens(addPens)
+
+  if (remove.length) {
+    const removeIds = []
+    // 找到所有子节点 id
+    remove.forEach((key) => {
+      const ids = pointIdKeyMap.value[key] || []
+      if (ids.length) {
+        removeIds.push(...ids)
+        const fn = (id) => {
+          const pen = meta2d.findOne(id)
+          if (pen && pen.children?.length)
+            pen.children.forEach(pid => removeIds.push(pid))
+        }
+        ids.forEach(fn)
+      }
+    })
+    const pens = selections.pens
+    meta2d.delete(removeIds.map((id) => {
+      return {
+        id,
+      }
+    }))
+    // 解决删除数据后失去焦点的问题
+    select(pens)
+  }
+  setSignalData() // 重置数据
+}
+</script>
+
 <template>
-  <div>1</div>
+  <div px-3 min-h-150px max-h-350px of-x-hidden of-y-auto border-box flex items-center justify-center>
+    <div v-if="!hasSeletedEquip" flex="~ col  gap-4" items-center justify-center>
+      <div i-solar:inbox-broken text-64px op-10 />
+      请先绑定设备
+    </div>
+
+    <div v-else flex="~ col gap-3" py-4>
+      <div grid="~ cols-2">
+        <div col-span-1 />
+        <div grid="~ cols-4">
+          <div v-for="i in 4" :key="i" col-span-1>
+            {{ ['A', 'V', 'D', 'T'][i - 1] }}
+          </div>
+        </div>
+      </div>
+      <el-checkbox-group v-model="selectedPointIds">
+        <div v-for="item in pointOptions" :key="item.value" grid="~ cols-2">
+          <el-checkbox :label="item.value" col-span-1>
+            {{ item.label }}
+          </el-checkbox>
+          <div col-span-1>
+            <el-checkbox-group v-model="selectedSignalComputedMap[item.value]" size="mini">
+              <div grid="~ cols-4" of-hidden>
+                <el-checkbox v-for="j in 4" :key="j" :label="['A', 'V', 'D', 'T'][j - 1]" />
+              </div>
+            </el-checkbox-group>
+          </div>
+        </div>
+      </el-checkbox-group>
+      <div flex-center>
+        <el-button type="primary" size="small" @click="autoGeneratePoints">
+          应用到画布
+        </el-button>
+        <el-button type="warning" size="small" @click="setSignalData">
+          重置
+        </el-button>
+      </div>
+    </div>
+  </div>
 </template>
