969126bd3024779b4127507fbda000d9f9683637
泠韵 <2138889191@qq.com>
2024-04-12 20:35:23 +0800

    chore: update

--- src/composables/meta2d/drag.ts
+++ src/composables/meta2d/drag.ts
@@ -1,3 +1,5 @@
+import { getDefaultPenColor } from '~/const'
+
 export function dragStart(e: DragEvent | MouseEvent, elem: any) {
   if (!elem)
     return
@@ -5,6 +7,15 @@ export function dragStart(e: DragEvent | MouseEvent, elem: any) {
   e.stopPropagation()
 
   const data = elem.data
+  const color = getDefaultPenColor()
+  const _data = Array.isArray(data)
+    ? [...data]
+    : [data]
+  _data.forEach((item) => {
+    if (!item.color)
+      item.color = color
+  })
+
   if (e instanceof DragEvent)
-    e.dataTransfer?.setData('Meta2d', JSON.stringify(data))
+    e.dataTransfer?.setData('Meta2d', JSON.stringify(_data))
 }
